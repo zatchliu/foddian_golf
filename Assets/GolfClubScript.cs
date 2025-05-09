b095f10305f32e4c17979d461be08d91272c730b
Zachary Liu <98660309+zatchliu@users.noreply.github.com>
2025-05-09 15:49:14 -0400

    Worked on Player Object and ClubScript

---
+++ Assets/GolfClubScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class GolfClubScript : MonoBehaviour
+{
+    public Transform pivotPoint;
+    public float radius = 1.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        var direction = mousePosition - pivotPoint.position;
+        if (direction.magnitude > radius)
+        {
+            direction.Normalize();
+            direction *= radius;
+            mousePosition = pivotPoint.position + direction;
+        }
+
+        GetComponent<Rigidbody2D>().MovePosition(mousePosition);
+    }
+}
